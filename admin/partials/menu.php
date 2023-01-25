a710d7e633fd7fc5ca30203be046bdc47719b806
gjylapulti <gp48517@ubt-uni.net>
2023-01-25 17:13:04 +0100

    Added manage pages

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -3,19 +3,19 @@
 <head>
     
     <title>Fresh Line - Home Page</title>
-    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="admin.css">
 </head>
 <body>
     <!--Menu Section Starts-->
     <div class="menu text-center">
         <div class="wrapper">
             <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="">Admin</a></li>
-                <li><a href="">Category</a></li>
-                <li><a href="">Food</a></li>
-                <li><a href="">Order</a></li>
-                <li><a href="">Logout</a></li>
+            <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+                <li><a href="logout.php">Logout</a></li>
             </ul>
         </div>
     </div>
