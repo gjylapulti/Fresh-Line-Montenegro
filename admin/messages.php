6c874f41afb9dea345c7002064cffa0b6b1dc581
gjylapulti <gp48517@ubt-uni.net>
2023-02-11 17:11:23 +0100

    Added Contact messages- admin

---
+++ admin/messages.php
@@ -0,0 +1,61 @@
+<?php
+include('partials/menu.php');
+
+//getting products from database in the hair category
+
+$sql2 = "SELECT * FROM message";
+
+$res2 = mysqli_query($conn, $sql2);
+
+$count2 = mysqli_num_rows($res2);
+
+$sn = 0;
+if ($count2 > 0) {
+    //we have products
+    while ($row = mysqli_fetch_assoc($res2)) {
+        $name = $row['name'];
+        $email = $row['email'];
+        $number = $row['number'];
+        $message = $row['message'];
+        $sn++;
+
+        ?>
+
+        <div class="container">
+
+            <div class="wrapper" style="border:1px solid black; margin:20px; display:flex; justify-content:space-around;">
+                <div class="information">
+                    <h3 style="color:#ff4757;">
+                        <?php echo $sn; ?>. Name:
+                        <?php echo $name ?>
+                    </h3>
+                    <h3>
+                        E-mail:
+                        <?php echo $email ?>
+                    </h3>
+                    <h3>
+                        Phone number:
+                        <?php echo $number ?>
+                    </h3>
+                </div>
+                <div class="information-message">
+                    <h3 style="color:#ff4757;">Message:
+
+                    </h3>
+                    <?php echo $message ?>
+                </div>
+            </div>
+
+        </div>
+
+
+        <?php
+    }
+} else {
+    echo "<div class='error'>Products not available</div>";
+}
+
+
+include('partials/footer.php');
+
+?>
--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -1,18 +1,19 @@
+<?php
 
-<?php 
-
-include('../config/constants.php'); 
+include('../config/constants.php');
 
 ?>
 
 
 
 <html>
+
 <head>
-    
+
     <title>Fresh Line Montenegro - Home Page</title>
     <link rel="stylesheet" href="../css/admin.css">
 </head>
+
 <body>-->
     <!--Menu Section Starts-->
     <div class="menu text-center">
@@ -21,11 +22,10 @@ include('../config/constants.php');
                 <li><a href="index.php">Home</a></li>
                 <li><a href="manage-category.php">Category</a></li>
                 <li><a href="manage-product.php">Product</a></li>
-                <li><a href="">Order</a></li>
+                <li><a href="messages.php">Messages</a></li>
                 <li><a href="../logout.php">Logout</a></li>
             </ul>
         </div>
     </div>
 
-        <!--Menu Section Ends-->
-
+    <!--Menu Section Ends-->
